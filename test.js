b4d6e8c052602782aee0a8fabe07f28d735f6148
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-04-28 11:11:34 +0545

    No await

--- test.js
+++ test.js
@@ -10,26 +10,26 @@ client.on("ready", () => {
 client.on("message", async (message) => {
     if (!message.guild || message.author.bot) return;
     if (message.content === "daily") {
-        let add = await eco.daily(message.author.id, 500);
+        let add = eco.daily(message.author.id, 500);
         if (add.onCooldown) return message.reply(`You already claimed your daily coins. Come back after ${add.time.days} days, ${add.time.hours} hours, ${add.time.minutes} minutes & ${add.time.seconds} seconds.`);
         else return message.reply(`you claimed ${add.amount} as your daily coins and now you have total ${add.after} coins.`);
     }
     if (message.content === "work") {
-        let add = await eco.work(message.author.id, 1000);
+        let add = eco.work(message.author.id, 1000);
         if (add.onCooldown) return message.reply(`You already worked. Come back after ${add.time.minutes} minutes & ${add.time.seconds} seconds.`);
         else return message.reply(`you worked ${add.amount} as a ${add.workedAs} and earned ${add.after} coins.`);
     }
     if (message.content === "weekly") {
-        let add = await eco.weekly(message.author.id, 500);
+        let add = eco.weekly(message.author.id, 500);
         if (add.onCooldown) return message.reply(`You already claimed your weekly coins. Come back after ${add.time.days} days, ${add.time.hours} hours, ${add.time.minutes} minutes & ${add.time.seconds} seconds.`);
         else return message.reply(`you claimed ${add.amount} as your weekly coins and now you have total ${add.after} coins.`);
     }
     if (message.content === "bal") {
-        let money = await eco.fetchMoney(message.author.id);
+        let money = eco.fetchMoney(message.author.id);
         return message.channel.send(`<@${money.user}> has ${money.amount} coins.`);
     }
     if (message.content === "leaderboard") {
-        let lb = await eco.leaderboard({ limit: 10, raw: false });
+        let lb = eco.leaderboard({ limit: 10, raw: false });
         const embed = new Discord.RichEmbed()
             .setAuthor("Leaderboard")
             .setColor("BLURPLE");
