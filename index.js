81d913ae49c4441a4d6ac1ce6d5e0b24dbb80791
zyrouge <naveencnaveen96@gmail.com>
2020-05-16 10:53:33 +0530

    Added ShopManager.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     Manager: require("./src/EconomyManager"),
+    ShopManager: require("./src/ShopManager"),
     version: require("./package.json").version,
     db: require("quick.db")
 }
