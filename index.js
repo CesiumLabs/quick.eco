7140534ce8bf3bbb707ea62219d5ba9b65df976a
zyrouge <naveencnaveen96@gmail.com>
2020-05-16 19:55:25 +0530

    GuildManager

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     Manager: require("./src/EconomyManager"),
+    GuildManager: require("./src/GuildManager"),
     ShopManager: require("./src/ShopManager"),
     version: require("./package.json").version,
     db: require("quick.db")
