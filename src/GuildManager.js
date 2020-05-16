f84eea621c40a9921d867ce327dc65977ceba978
ZYROUGE <me@zyrouge.gq>
2020-05-16 20:54:12 +0530

    EconomyManager to GuildManager

--- src/GuildManager.js
+++ src/GuildManager.js
@@ -2,7 +2,7 @@ let db = require("quick.db");
 let invalid = [0, -1, -0];
 let invL = [-1, -0];
 
-class EconomyManager {
+class GuildManager {
 
     /**
      * @constructor
@@ -354,4 +354,4 @@ class EconomyManager {
     }
 }
 
-module.exports = EconomyManager;
+module.exports = GuildManager;
