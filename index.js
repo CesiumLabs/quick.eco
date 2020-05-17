d0c691511f4525fb19ac3a382e5363dc26d20bd1
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-05-17 09:29:45 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
     Manager: require("./src/EconomyManager"),
     GuildManager: require("./src/GuildManager"),
     ShopManager: require("./src/ShopManager"),
+    LotteryManager: require("./src/LotteryManager"),
     version: require("./package.json").version,
     db: require("quick.db")
-}
+};
