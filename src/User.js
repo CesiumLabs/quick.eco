acaaeb7798f46766be77133fcdaeb976bbdd14a4
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-03 17:53:16 +0545

    Create User.js

---
+++ src/User.js
@@ -0,0 +1,18 @@
+class User {
+    
+    constructor(id, guild, database) {
+        this.id = id;
+        this.guild = guild;
+        this.db = database;
+    }
+
+    get balance() {
+        if (this.guild) {
+            let i = this.db.get(`money_${this.guild}_${this.id}`)
+            return (i ? i : 0);
+        }
+        return (this.db.get(`money_${this.id}`) ? this.db.get(`money_${this.id}`) : 0;
+    }
+}
+
+module.exports = User;
