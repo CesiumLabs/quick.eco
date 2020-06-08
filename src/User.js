e2e3c8dd6625694cc6640c9006ec67d5b1588404
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-08 09:19:31 +0545

    Update User.js

--- src/User.js
+++ src/User.js
@@ -3,11 +3,17 @@ class User {
     constructor(id, guild, database) {
         this.id = id;
         if (guild && typeof guild == "string") this.guild = guild;
-        if (guild && typeof guild == "object" && !database) this.db = guild;
-        else this.database = database;
+        if (guild && typeof guild !== "string" && !database) this.db = guild;
+        this.db = database;
     }
 
+    /**
+      * Balance of the user
+      * @type Number
+      * @returns {Balance}
+      */
     static get balance() {
+        if (!this.db) throw new Error("Partial users may not have balance");
         if (this.guild) {
             let i = this.db.get(`money_${this.guild}_${this.id}`)
             return (i || 0);
@@ -15,11 +21,21 @@ class User {
         return (this.db.get(`money_${this.id}`) || 0);
     }
 
+    /**
+      * create mention
+      * @type String
+      * @returns String
+      */
     static toString() {
         return `<@${this.id}>`;
     }
 
+    /**
+      * parses everything related to user
+      * @returns data[]
+      */
     static parseAll() {
+        if (!this.db) throw new Error("Partial users may not have balance.");
         let data = this.db.all().filter(i => i.ID.includes(this.id));
         return data ? (data.length ? data : []): [];
     }
