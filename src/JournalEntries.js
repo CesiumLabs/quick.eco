840922f1c4ad747268d7603dd1b216aff4ae5c8c
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-09 10:25:54 +0545

    Update JournalEntries.js

--- src/JournalEntries.js
+++ src/JournalEntries.js
@@ -1,5 +1,5 @@
 /* 
-Not actually Journal Entries but 
+Not actual Journal Entries but 
 this code will keep record of each 
 transactions as per the current month
 */
@@ -26,24 +26,35 @@ class Journal {
              "November",
              "December"
        ];
-       this.date = new Date();
     }
 
     static prepare(user, amount, ...details) {
         if (!user || !amount) throw new EcoError("User or amount details missing in statement");
         if (!this._resolveUser(user)) throw new EcoError("Invalid user details.");
         if (typeof amount !== "number") throw new EcoError("Type of amount must be a number");
-        return this.db.set(`stmt_${this.date.getFullYear()}_${this.months[this.date.getMonth()]}`, {
-            id: this.db.all().filter(ent => ent.startsWith("stmt")).length + 1,
-            author: user,
+        const entries = this.db.all().filter(ent => ent.startsWith("stmt")).length + 1;
+        return this.db.set(`stmt_${new Date().getFullYear()}_${this.months[new Date().getMonth()]}_${ID}`, {
+            id: entries,
+            author: new User(user),
             amount: amount,
+            month: this.months[new Date().getMonth()],
             entryAt: new Date(),
             data: ...details || {}
         });
     }
 
-    static fetch(user) {
+    all() {
+       return this.db.all().filter(i => i.ID.startsWith("stmt_"));
+    }
 
+    filter(...args) {
+        return this.db.all().filter(...args);
+    }
+
+    fetchEntries(user) {
+        if (!this._resolveUser(user)) throw new Error("Invalid User");
+        let statements = this.db.all().filter(i => i.ID.startsWith("stmt_") && i.data.author.id === user).reverse();
+        return statements;
     }
 
     static clear() {
