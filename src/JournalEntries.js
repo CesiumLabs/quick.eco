fafb509d67ffeae9f223c3adeea774623f2594f2
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-09 09:59:50 +0545

    Update JournalEntries.js

--- src/JournalEntries.js
+++ src/JournalEntries.js
@@ -1,13 +1,17 @@
+/* 
+Not actually Journal Entries but 
+this code will keep record of each 
+transactions as per the current month
+*/
+const User = require("./User");
 const EcoError = require("./Error");
 const db = require("rex.db");
 db.init("./economy");
 
 class Journal {
     
-    constructor(root) {
-        if (typeof root !== "string") throw new EcoError("Root name must be a string");
-        let currentMonth = new Date().getMonth() + 1;
-        this.db = db.table(`transactions_${currentMonth}`);
+    constructor() {
+        this.db = db.table(`transactions`);
         this.months = [
              "January",
              "February",
@@ -25,13 +29,31 @@ class Journal {
        this.date = new Date();
     }
 
-    static prepare(dr, cr, type) {
-        if (!dr || !cr) throw new EcoError("Debit or Credit details missing in statement");
-        if (!type) throw new EcoError("No type provided");
-        if (typeof dr !== "object" || typeof cr !== "object") throw new EcoError("Invalid data type provided for debit or credit.");
-        if (typeof type !== "string") throw new EcoError("Type of type must be a string");
-        
-        // todo: add entries
+    static prepare(user, amount, ...details) {
+        if (!user || !amount) throw new EcoError("User or amount details missing in statement");
+        if (!this._resolveUser(user)) throw new EcoError("Invalid user details.");
+        if (typeof amount !== "number") throw new EcoError("Type of amount must be a number");
+        return this.db.set(`stmt_${this.date.getFullYear()}_${this.months[this.date.getMonth()]}`, {
+            id: this.db.all().filter(ent => ent.startsWith("stmt")).length + 1,
+            author: user,
+            amount: amount,
+            entryAt: new Date(),
+            data: ...details || {}
+        });
+    }
+
+    static fetch(user) {
+
+    }
+
+    static clear() {
+        return this.db.deleteAll();
+    }
+
+    static _resolveUser(user) {
+        if (typeof user === "string") user = new User(user);
+        if (!(user instanceof User)) return false;
+        return true;
     }
     
 }
