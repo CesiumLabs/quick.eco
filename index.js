5e9a9eee11c6c593cb3c89dc231eef94e3bff8a9
Snowflake107 <devsnowflake107@gmail.com>
2020-09-19 11:40:35 +0545

    Export Util

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     EconomyManager: require("./src/Eco"),
     Manager: require("./src/Manager"),
+    Util: require("./src/Util"),
     version: require("./package.json").version
 };
